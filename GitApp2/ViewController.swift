62c1d814b92faa9675787c34f5c415a7b1e3c20e
藤田優作 <marozaemon1217@gmail.com>
2020-12-28 00:57:51 +0900

    commit name

---
+++ GitApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitApp2
+//
+//  Created by 藤田優作 on 2020/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
